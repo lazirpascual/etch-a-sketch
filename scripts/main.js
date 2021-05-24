12cd2c972d17916531aaf77ae70d360d8bc24a84
Lazir Pascual <lazirpascual@gmail.com>
2021-05-24 02:46:27 -0400

    add ability to change block size

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,38 @@
-/* Global Variables */
-const MAX_BLOCK = 16;
-
+// global queries
 const blockContainer = document.querySelector('#main');
 
-// create an individual block
-function createBlock() {
-    const squareBlock = document.createElement('div');
-    squareBlock.classList.add('block-container');
-    blockContainer.appendChild(squareBlock);
-}
+// create 16x16 blocks upon loading page
+let INITIAL_SIZE = 16;
+createBlock(INITIAL_SIZE);
 
-for (let i = 0; i < MAX_BLOCK; i++) {
-    for (let j = 0; j < MAX_BLOCK; j++) { 
-    createBlock();
+// create the full block given the size
+function createBlock(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) { 
+            const squareBlock = document.createElement('div');
+            squareBlock.classList.add('block-container');
+            blockContainer.appendChild(squareBlock);
+        }
+        blockContainer.style.gridTemplateColumns=`repeat(${size},auto)`;
     }
 }
 
-// change color to grey on hover
-blockContainer.addEventListener('mouseover', function (e){
+// removes all existing child nodes in specified node
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// change color to grey on mouse hover
+blockContainer.addEventListener('mouseover', function (e) {
     e.target.classList.add('grey-box');
 });
 
-
-
+// change size of block on button click
+const sizeButton = document.querySelector('#switch');
+sizeButton.addEventListener('click', function (e) {
+    let blockSize = prompt("Enter the Grid Size: ");
+    removeAllChildNodes(blockContainer);
+    createBlock(blockSize);
+});
