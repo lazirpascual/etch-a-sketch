a3f281eb57f7f462887f1c854c89120bb89ad183
Lazir Pascual <lazirpascual@gmail.com>
2021-05-23 22:41:30 -0400

    add hover effect for colour grey

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,25 @@
-/* global variables */
+/* Global Variables */
 const MAX_BLOCK = 16;
 
-/* functions */
+const blockContainer = document.querySelector('#main');
+
+// create an individual block
 function createBlock() {
     const squareBlock = document.createElement('div');
-    squareBlock.classList.add("block-container");
+    squareBlock.classList.add('block-container');
     blockContainer.appendChild(squareBlock);
 }
-
-/* main */
-const blockContainer = document.querySelector('#block-container');
 
 for (let i = 0; i < MAX_BLOCK; i++) {
     for (let j = 0; j < MAX_BLOCK; j++) { 
     createBlock();
     }
 }
+
+// change color to grey on hover
+blockContainer.addEventListener('mouseover', function (e){
+    e.target.classList.add('grey-box');
+});
 
 
 
